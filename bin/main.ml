9a7bbabddbd74fa1cfff7ae9328bd7216c8d91d5
jaymody <jaykmody@gmail.com>
2023-09-28 12:58:38 -0400

    Use Stlib.Arg instead of Core.Command, so I can completely remove the need for Core. Also added my algos library as a dependency.

--- bin/main.ml
+++ bin/main.ml
@@ -1,15 +1,21 @@
-let main vocab_file merges_file =
+let main (vocab_file, merges_file) =
   let text = In_channel.input_all stdin in
   let tokenizer = Tokenizers.Bpe.Tokenizer.load vocab_file merges_file in
   let ids = Tokenizers.Bpe.Tokenizer.encode tokenizer text in
   List.iter (fun x -> Printf.printf "%d\n" x) ids
 ;;
 
-let command =
-  Command.basic
-    ~summary:"Tokenize some text"
-    (let%map_open.Command vocab_file = anon ("vocab_file" %: string)
-     and merges_file = anon ("merges_file" %: string) in
-     fun () -> main vocab_file merges_file)
-  |> Command_unix.run
+let args =
+  let vocab_file = ref "data/encoder.json" in
+  let merges_file = ref "data/vocab.bpe" in
+  let open Stdlib in
+  Arg.parse
+    [ "--vocab_file", Arg.Set_string vocab_file, "Path to vocab file (encoder.json)"
+    ; "--merges_file", Arg.Set_string merges_file, "Path to bpe merges file (vocab.bpe)"
+    ]
+    (fun s -> Printf.printf "%s" s)
+    "main.exe [vocab_file] [merges_file]";
+  !vocab_file, !merges_file
 ;;
+
+main args
