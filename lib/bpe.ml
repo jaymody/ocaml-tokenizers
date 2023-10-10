8857a9d8f1be810a74e43c08f8089f45d68d82f6
jaymody <jaykmody@gmail.com>
2023-10-10 13:37:54 -0400

    Initial implementation of BPE using an AVL tree + priority queue.

--- lib/bpe.ml
+++ lib/bpe.ml
@@ -52,39 +52,131 @@ let decode_visible_unicode_to_bytes text =
     text
 ;;
 
-let get_min_pair tokens ranks =
-  let rec aux min_pair = function
-    | a :: (b :: _ as tl) ->
-      let new_min_pair =
-        match min_pair with
-        | None -> if Hashtbl.mem ranks (a, b) then Some (a, b) else None
-        | Some p ->
-          (match Hashtbl.find_opt ranks (a, b) with
-           | Some r when r < Hashtbl.find ranks p -> Some (a, b)
-           | _ -> Some p)
-      in
-      aux new_min_pair tl
-    | _ -> min_pair
-  in
-  aux None tokens
-;;
+let[@warning "-32-26"] bpe text ranks =
+  let module Token = struct
+    type t =
+      { i : int
+      ; j : int
+      }
 
-let merge_tokens tokens pair =
-  let rec aux acc = function
-    | a :: b :: tl when (a, b) = pair -> aux ((a ^ b) :: acc) tl
-    | a :: tl -> aux (a :: acc) tl
-    | _ -> acc
-  in
-  List.rev (aux [] tokens)
-;;
+    let create i j = { i; j }
+    let compare a b = if a.i = b.i then a.j - b.j else a.i - b.i
+    let to_str { i; j } = String.sub text i (j - i)
 
-let bpe text ranks =
-  let rec aux tokens =
-    match get_min_pair tokens ranks with
-    | None -> tokens
-    | Some pair -> aux (merge_tokens tokens pair)
+    let merge a b =
+      assert (a.j = b.i);
+      { i = a.i; j = b.j }
+    ;;
+  end
   in
-  Utils.str_to_utf8_list text |> aux
+  let module Pair = struct
+    type t =
+      { l : Token.t
+      ; r : Token.t
+      ; score : int
+      }
+
+    let create l r =
+      match Hashtbl.find_opt ranks (Token.to_str l, Token.to_str r) with
+      | None -> None
+      | Some score -> Some { l; r; score }
+    ;;
+
+    let compare { score = score1; _ } { score = score2; _ } = score1 - score2
+    let merge { l; r; _ } = Token.merge l r
+
+    let to_str { l; r; score } =
+      "(" ^ Token.to_str l ^ ", " ^ Token.to_str r ^ ", " ^ Int.to_string score ^ ")"
+    ;;
+  end
+  in
+  let module PairPq = Algos.Pq.Make (Pair) in
+  let module Tokens = Algos.Avl.Make (Token) in
+  let init_tokens =
+    let len = String.length text in
+    let rec aux i acc =
+      let j = BatUTF8.next text i in
+      if j < len
+      then aux j (Tokens.insert (Token.create i j) "throwaway" acc)
+      else Tokens.insert (Token.create i j) "throwaway" acc
+    in
+    aux 0 Tokens.empty
+  in
+  let tokens_to_str tokens =
+    tokens
+    |> Tokens.to_list
+    |> List.map fst
+    |> List.map Token.to_str
+    |> List.iter (fun s -> Printf.printf "%s " s);
+    print_endline ""
+  in
+  let pq_to_str pq =
+    PairPq.to_list pq
+    |> List.map Pair.to_str
+    |> List.iter (fun s -> Printf.printf "%s " s);
+    print_endline ""
+  in
+  let push_pair_opt pq = function
+    | None -> pq
+    | Some p -> PairPq.push p pq
+  in
+  let init_pq =
+    let rec aux pq = function
+      | a :: (b :: _ as tl) ->
+        aux
+          (match Pair.create a b with
+           | None -> pq
+           | Some pair -> PairPq.push pair pq)
+          tl
+      | _ -> pq
+    in
+    Tokens.to_list init_tokens |> List.map fst |> aux PairPq.empty
+  in
+  let update_prev_next (new_token : Token.t) tokens pq =
+    let rec update_prev pq = function
+      | Tokens.Empty -> pq
+      | Tokens.Node n ->
+        if new_token.i = n.k.j
+        then push_pair_opt pq (Pair.create n.k new_token)
+        else if new_token.i < n.k.i
+        then update_prev pq n.l
+        else update_prev pq n.r
+    in
+    let rec update_next pq = function
+      | Tokens.Empty -> pq
+      | Tokens.Node n ->
+        if new_token.j = n.k.i
+        then push_pair_opt pq (Pair.create new_token n.k)
+        else if new_token.i < n.k.i
+        then update_next pq n.l
+        else update_next pq n.r
+    in
+    let pq = update_prev pq tokens in
+    update_next pq tokens
+  in
+  let merge_pair tokens pq (pair : Pair.t) =
+    if Option.is_some (Tokens.find pair.l tokens)
+       && Option.is_some (Tokens.find pair.r tokens)
+    then (
+      let tokens = tokens |> Tokens.pop pair.l |> snd |> Tokens.pop pair.r |> snd in
+      let new_token = Pair.merge pair in
+      let tokens = Tokens.insert new_token "throwaway" tokens in
+      let pq = update_prev_next new_token tokens pq in
+      tokens_to_str tokens;
+      pq_to_str pq;
+      print_endline "-----";
+      tokens, pq)
+    else tokens, pq
+  in
+  let rec aux (tokens, pq) =
+    match PairPq.pop pq with
+    | None, _ -> tokens
+    | Some pair, pq -> aux (merge_pair tokens pq pair)
+  in
+  tokens_to_str init_tokens;
+  pq_to_str init_pq;
+  print_endline "-----";
+  aux (init_tokens, init_pq) |> Tokens.to_list |> List.map fst |> List.map Token.to_str
 ;;
 
 module Tokenizer : sig
